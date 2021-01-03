f9e5b6360f7671b5b3b2755b2424e561db5b8562
unihit <unihit@naver.com>
2021-01-03 23:25:50 +0900

    비밀번호 암호화, 로그인 기능, 토큰 생성, Auth 기능

--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
   name: {
@@ -12,7 +15,7 @@ const userSchema = mongoose.Schema({
   },
   password: {
     type: String,
-    maxlength: 50
+    maxlength: 70
   },
   role: {
     type: Number,
@@ -26,6 +29,61 @@ const userSchema = mongoose.Schema({
     type: Number
   }
 })
+
+userSchema.pre('save', function(next){
+  let user = this;
+
+  // 비밀번호가 변경될 때만 암호화를 해준다.
+  if(user.isModified('password')) {
+    // 비밀번호를 암호화 시킨다.
+    bcrypt.genSalt(saltRounds, function(err, salt) {
+      if(err) return next(err)
+      bcrypt.hash(user.password, salt, function(err, hash) {
+        if(err) return next(err)
+        user.password = hash
+        next()
+      })
+    })
+  } else {
+    next()
+  }
+})
+
+userSchema.methods.comparePassword = function(plainPassword, cb) {
+  // painPassword  1234567 암호화된 비밀번호  $2b$10$B0wKNoPhQkHDDCGeCC1NJ.gpEsG1hISL2Z/KOvYwJ.Wnex9muZOSu
+  bcrypt.compare(plainPassword, this.password, function(err, isMatch) {
+    if(err) return cb(err)
+    cb(null, isMatch)
+  })
+}
+
+userSchema.methods.generateToken = function(cb) {
+  let user = this;
+  // jsonwebtoken을 이용해서 token을 생성하기
+  let token = jwt.sign(user._id.toHexString(), 'secretToken')
+  // user._id + 'secretToken' = token
+  // ->
+  // 'secretToken' -> user._id
+  user.token = token
+  user.save(function(err, user) {
+    if(err) return cb(err)
+    cb(null, user)
+  })
+}
+
+userSchema.statics.findByToken = function(token, cb) {
+  let user = this;
+
+
+  // 토큰을 decode 한다.
+  jwt.verify(token, 'secretToken', function(err, decoded) {
+    // 유저 아이디를 이요해서 유저를 찾은 다음 클라이언트에서 가져온 토큰과 DB에 보관된 토큰이 일치하는지 확인
+    user.findOne({"_id": decoded, "token": token}, function(err, user){
+      if(err) return cb(err);
+      cb(null, user)
+    })
+  });
+}
 
 const User = mongoose.model('User', userSchema)
 
