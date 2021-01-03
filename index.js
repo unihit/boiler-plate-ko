6aedef039e2f6d32d47bfadf3215cd46e9c585cc
unihit <unihit@naver.com>
2021-01-03 19:21:25 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require("body-parser");
+
+const config = require('./config/key')
+
+const {User} = require("./models/User");
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+// aplication/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
 mongoose
-  .connect(
-    "mongodb://unihit:wlgnlwk1004@boilerplate-shard-00-00.eaiaz.mongodb.net:27017,boilerplate-shard-00-01.eaiaz.mongodb.net:27017,boilerplate-shard-00-02.eaiaz.mongodb.net:27017/<dbname>?ssl=true&replicaSet=atlas-gd0g3z-shard-0&authSource=admin&retryWrites=true&w=majority",
+  .connect(config.mongoURI,
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
@@ -18,6 +27,17 @@ mongoose
 
 
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World! ~실시간으로 바꾸기~'))
+
+app.post('/register', (req, res)=> {
+  // 회원 가입할 때 필요한 정보들을 client에서 가져오면 그것들을 DB에 넣어준다.
+  const user = new User(req.body)
+  user.save((err,doc) => {
+    if(err) return res.json({sucess: false, })
+    return res.status(200).json({
+      sucess: true
+    })
+  })
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
